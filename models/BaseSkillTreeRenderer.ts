738c3f9cbb89f0e2b4076c07c9f936e0be1756ca
Lilylicious <lily.hammerskog@gmail.com>
2023-10-07 15:16:48 +0200

    Change back to tealish for desired

--- models/BaseSkillTreeRenderer.ts
+++ models/BaseSkillTreeRenderer.ts
@@ -675,7 +675,7 @@ export enum HighlightColor {
     Removed = 0xFF0000,
     Moved = 0xFF1493,
     Searched = 0xB452FF,
-    Desired = 0x0000FF,
+    Desired = 0x40E0D0,
     UnDesired = 0x800020,
 };
 
